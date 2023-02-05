7cd675a20be97874e0978b5976f206cdfecf064a
Hamzat <hgeroev11@mail.com>
2023-02-06 00:23:29 +0300

    Задание 2 готово

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Promt(string? message)
+{
+    System.Console.Write($"{message} -> ");
+    return int.Parse(Console.ReadLine());
+}
+
+int Sum(int nums)
+{
+    int result = 0;
+    while (nums > 0)
+    {
+        int temp = nums % 10;
+        nums = nums / 10;
+        result = result + temp;
+    }
+    return result;
+}
+
+int number = Promt("Введите число");
+System.Console.WriteLine($"Сумма цифр в числе {number} = {Sum(number)}");
