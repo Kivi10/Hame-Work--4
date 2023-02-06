e4029a1d1b3906a545a744797a42a28c00e562b1
Hamzat <hgeroev11@mail.com>
2023-02-06 03:40:16 +0300

    Задание 2 готово

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,14 +1,5 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Сделать через функции 
 
-// int[] array = new int[8];
-// Random rand = new Random();
-
-// for (int x = 0; x < array.Length; x++)
-// {
-//     array[x] = rand.Next(2);
-//     System.Console.Write(array[x] + "; ");
-// }
-
 int Promt(string? message)
 {
     System.Console.Write(message + "-> ");
